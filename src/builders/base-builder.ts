c54056d1e8edb45cb1d4e84be97078d1877bfe64
Ruben Gomes <rbnpontes@gmail.com>
2023-05-25 08:35:29 -0300

    remove unused builders

--- src/builders/base-builder.ts
+++
@@ -1,8 +0,0 @@
-import CodeBuilder from "../code-builder";
-
-export interface IBuilder<T> {
-    build() : T;
-}
-export default class BaseBuilder {
-    constructor(protected code : CodeBuilder){}
-}
